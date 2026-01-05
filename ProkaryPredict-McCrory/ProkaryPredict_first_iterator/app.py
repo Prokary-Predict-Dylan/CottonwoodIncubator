73b359b24b3c8d86948b752b3153e55f33ec4edf
Prokary-Predict-Dylan <prokarypredict@gmail.com>
2026-01-04 21:11:55 -0700

    Change Streamlit page title for testing

--- ProkaryPredict-McCrory/ProkaryPredict_first_iterator/app.py
+++ ProkaryPredict-McCrory/ProkaryPredict_first_iterator/app.py
@@ -9,7 +9,7 @@ import io, base64, time
 # ---------------------------
 # Streamlit page config
 # ---------------------------
-st.set_page_config(page_title="ProkaryPredict First Iterator", layout="wide")
+st.set_page_config(page_title="ProkaryPredict First Iterator TEST", layout="wide")
 st.title("ProkaryPredict — (First Iterator)")
 
 # Initialize export_request
